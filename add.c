56a10727589bf8379aa7c0ecf38947649568061a
Z-eng-zi <m19884272312@163.com>
2025-03-21 17:14:22 +0800

    first-commit

---
+++ add.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+int Add(int x, int y)
+{
+    int z = x + y;
+    return z;
+}
+
+int main()
+{
+    int num1 = 0;
+    int num2 = 0;
+    int sum = 0;
+    printf("输入两个操作数:>");
+    scanf("%d %d", &num1, &num2);
+    sum = Add(num1, num2);
+    printf("sum = %d\n", sum);
+    return 0;
+}
